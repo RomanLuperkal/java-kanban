7e4207887a960df1365284bf140c22c69dbe3b6d
Roman <madara-09@bk.ru>
2022-06-02 15:12:28 +0300

    fix: реализовано извлечение и обновление объектов задач

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import Tasks.Status;
 import java.lang.NullPointerException;
 
 public class Main implements TestObjects{
 
     public static void main(String[] args) {
         Manager manager = new Manager();
+
         try {
             manager.createSimpleTask(simpleTask);
             manager.createEpicTask(epicTask1);
@@ -19,31 +21,37 @@ public class Main implements TestObjects{
             System.out.println("Список всех простых задач:");
             System.out.println(manager.getSimpleTasks());
             System.out.println("Обновление статуса простой задачи до IN_PROGRESS:");
-            manager.updateSimpleTask(simpleTask.getId(), updateSimpleTask1);
+            manager.updateSimpleTask(simpleTask.getId(),
+                    manager.getSimpleTask(simpleTask.getId()).changeStatus(Status.IN_PROGRESS));
             System.out.println(manager.getSimpleTasks());
             System.out.println("Обновление статуса простой задачи до DONE:");
-            manager.updateSimpleTask(updateSimpleTask1.getId(),updateSimpleTask2);
+            manager.updateSimpleTask(simpleTask.getId(),
+                    manager.getSimpleTask(simpleTask.getId()).changeStatus(Status.DONE));
             System.out.println(manager.getSimpleTasks());
             System.out.println("Обновление 1й подзадачи до статуса IN_PROGRESS у 1й эпической задачи");
-            manager.updateSubtask(epicTask1.getId(), subtask1ForEpicTask1.getId(), update1Subtask1ForEpicTask1);
+            manager.updateSubtask(epicTask1.getId(), subtask1ForEpicTask1.getId(),
+                    manager.getSubtask(subtask1ForEpicTask1.getId()).changeStatus(Status.IN_PROGRESS));
             System.out.println(manager.getEpicTask(epicTask1.getId()));
             System.out.println("Обновление 2й подзадачи до статуса IN_PROGRESS у 1й эпической задачи");
-            manager.updateSubtask(epicTask1.getId(), subtask2ForEpicTask1.getId(), update1Subtask2ForEpicTask1);
+            manager.updateSubtask(epicTask1.getId(), subtask2ForEpicTask1.getId(),
+                    manager.getSubtask(subtask2ForEpicTask1.getId()).changeStatus(Status.IN_PROGRESS));
             System.out.println(manager.getEpicTask(epicTask1.getId()));
             System.out.println("Обновление 1й подзадачи до статуса DONE у 1й эпической задачи");
-            manager.updateSubtask(epicTask1.getId(), update1Subtask1ForEpicTask1.getId(), update2Subtask1ForEpicTask1);
+            manager.updateSubtask(epicTask1.getId(), subtask1ForEpicTask1.getId(),
+                    manager.getSubtask(subtask1ForEpicTask1.getId()).changeStatus(Status.DONE));
             System.out.println(manager.getEpicTask(epicTask1.getId()));
             System.out.println("Обновление 2й подзадачи до статуса DONE у 1й эпической задачи");
-            manager.updateSubtask(epicTask1.getId(), update1Subtask2ForEpicTask1.getId(), update2Subtask2ForEpicTask1);
+            manager.updateSubtask(epicTask1.getId(), subtask2ForEpicTask1.getId(),
+                    manager.getSubtask(subtask2ForEpicTask1.getId()).changeStatus(Status.DONE));
             System.out.println(manager.getEpicTask(epicTask1.getId()));
             System.out.println("Удаление подзадачи из 1й эпической задачи");
-            manager.deleteSubtask(epicTask1.getId(), update2Subtask2ForEpicTask1.getId());
+            manager.deleteSubtask(epicTask1.getId(), subtask1ForEpicTask1.getId());
             System.out.println(manager.getEpicTask(epicTask1.getId()));
             System.out.println("Удаление 2й эпической задачи");
             manager.deleteEpicTask(epicTask2.getId());
             System.out.println(manager.getEpicTasks());
             System.out.println("Удаление простой задачи");
-            manager.deleteSimpleTask(updateSimpleTask2.getId());
+            manager.deleteSimpleTask(simpleTask.getId());
             System.out.println(manager.getSimpleTasks());
         }catch (NullPointerException e) {
             System.out.println(e.getMessage());
