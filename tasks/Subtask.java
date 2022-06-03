2be0dc48b12b774dc1be97cc23e585e853ea42df
RomanLuperkal <104627970+RomanLuperkal@users.noreply.github.com>
2022-06-03 13:19:14 +0300

    Add files via upload
    -

---
+++ tasks/Subtask.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public class Subtask extends Task {
+    public Subtask(String name, String description) {
+
+        super(name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
