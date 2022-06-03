b739329e9bbda46bbb66fe84ae410a585f507494
Roman <madara-09@bk.ru>
2022-06-03 12:22:41 +0300

    style: переименован объект

--- src/TestObjects.java
+++ src/TestObjects.java
@@ -1,4 +1,4 @@
-import Tasks.*;
+import tasks.*;
 
 public interface TestObjects {
     EpicTask epicTask1 = new EpicTask("EpicTask 1", "Description epic task");
@@ -6,6 +6,6 @@ public interface TestObjects {
     Subtask subtask1ForEpicTask1 = new Subtask("sub1 for ep1", "Description sub1 for ep 1");
     Subtask subtask2ForEpicTask1 = new Subtask("sub2 for ep1", "Description sub2 for ep 1");
     Subtask subtask1ForEpicTask2 = new Subtask("sub1 for ep2", "Description sub1 for ep 2");
-    SimpleTask simpleTask = new SimpleTask("SimpleTask", "Description SimpleTask");
+    Task task = new Task("Task", "Description Task");
 
 }
