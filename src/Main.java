6382ea9a0606743a0202113118c1a8237e038b95
Roman <madara-09@bk.ru>
2022-06-13 22:05:29 +0300

    refactor: тестирование обновленного таск менеджера

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,38 @@
-import tasks.Status;
+import tasks.Task;
+
 import java.lang.NullPointerException;
 
-public class Main implements TestObjects{
+public class Main implements TestObjects {
 
     public static void main(String[] args) {
-        Manager manager = new Manager();
-
+        TaskManager inMemoryTaskManager = Managers.getDefault();
         try {
-            manager.createTask(task);
-            manager.createEpicTask(epicTask1);
-            manager.createSubtask(epicTask1.getId(), subtask1ForEpicTask1);
-            manager.createSubtask(epicTask1.getId(), subtask2ForEpicTask1);
-            manager.createEpicTask(epicTask2);
-            manager.createSubtask(epicTask2.getId(), subtask1ForEpicTask2);
+            inMemoryTaskManager.createTask(task);
+            inMemoryTaskManager.createEpicTask(epicTask1);
+            inMemoryTaskManager.createSubtask(epicTask1.getId(), subtask1ForEpicTask1);
+            inMemoryTaskManager.createSubtask(epicTask1.getId(), subtask2ForEpicTask1);
+            inMemoryTaskManager.createEpicTask(epicTask2);
+            inMemoryTaskManager.createSubtask(epicTask2.getId(), subtask1ForEpicTask2);
 
-            System.out.println("Список всех подзадач:");
-            System.out.println(manager.getSubtasks());
-            System.out.println("Список всех эпических задач:");
-            System.out.println(manager.getEpicTasks());
-            System.out.println("Список всех простых задач:");
-            System.out.println(manager.getTasks());
-            System.out.println("Обновление статуса простой задачи до IN_PROGRESS:");
-            task.changeStatus(Status.IN_PROGRESS);
-            manager.updateTask(task);
-            System.out.println(manager.getTasks());
-            System.out.println("Обновление статуса простой задачи до DONE:");
-            task.changeStatus(Status.DONE);
-            manager.updateTask(task);
-            System.out.println(manager.getTasks());
-            System.out.println("Обновление 1й подзадачи до статуса IN_PROGRESS у 1й эпической задачи");
-            subtask1ForEpicTask1.changeStatus(Status.IN_PROGRESS);
-            manager.updateSubtask(subtask1ForEpicTask1);
-            System.out.println(manager.getEpicTask(epicTask1.getId()));
-            System.out.println("Обновление 2й подзадачи до статуса IN_PROGRESS у 1й эпической задачи");
-            subtask2ForEpicTask1.changeStatus(Status.IN_PROGRESS);
-            manager.updateSubtask(subtask2ForEpicTask1);
-            System.out.println(manager.getEpicTask(epicTask1.getId()));
-            System.out.println("Обновление 1й подзадачи до статуса DONE у 1й эпической задачи");
-            subtask1ForEpicTask1.changeStatus(Status.DONE);
-            manager.updateSubtask(subtask1ForEpicTask1);
-            System.out.println(manager.getEpicTask(epicTask1.getId()));
-            System.out.println("Обновление 2й подзадачи до статуса DONE у 1й эпической задачи");
-            subtask2ForEpicTask1.changeStatus(Status.DONE);
-            manager.updateSubtask(subtask2ForEpicTask1);
-            System.out.println(manager.getEpicTask(epicTask1.getId()));
-            System.out.println("Удаление подзадачи из 1й эпической задачи");
-            manager.deleteSubtask(subtask1ForEpicTask1.getId());
-            System.out.println(manager.getEpicTask(epicTask1.getId()));
-            System.out.println("Удаление 2й эпической задачи");
-            manager.deleteEpicTask(epicTask2.getId());
-            System.out.println(manager.getEpicTasks());
-            System.out.println("Удаление простой задачи");
-            manager.deleteTask(task.getId());
-            System.out.println(manager.getTasks());
-        }catch (NullPointerException e) {
+            System.out.println(inMemoryTaskManager.getTask(task.getId()));
+            System.out.println(inMemoryTaskManager.getEpicTask(epicTask1.getId()));
+            System.out.println(inMemoryTaskManager.getEpicTask(epicTask2.getId()));
+            System.out.println(inMemoryTaskManager.getSubtask(subtask1ForEpicTask1.getId()));
+            System.out.println(inMemoryTaskManager.getSubtask(subtask2ForEpicTask1.getId()));
+            System.out.println(inMemoryTaskManager.getSubtask(subtask1ForEpicTask2.getId()));
+            System.out.println(inMemoryTaskManager.getTask(task.getId()));
+            System.out.println(inMemoryTaskManager.getTask(task.getId()));
+            System.out.println(inMemoryTaskManager.getTask(task.getId()));
+            System.out.println(inMemoryTaskManager.getTask(task.getId()));
+            System.out.println(inMemoryTaskManager.getTask(task.getId()));
+            System.out.println(inMemoryTaskManager.getEpicTask(epicTask1.getId()));
+
+            System.out.println("История просмотра:");
+            int i = 1;
+            for (Task task : inMemoryTaskManager.getHistory()) {
+                System.out.println(i++ + ": " + task);
+            }
+        } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         }
     }
