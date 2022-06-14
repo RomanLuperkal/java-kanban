7f853a49eacf28f35b16aaec3219d65a1d9fba8e
Roman <madara-09@bk.ru>
2022-06-14 19:49:12 +0300

    refactor: изменен тип переменных epicTask

--- src/TestObjects.java
+++ src/TestObjects.java
@@ -1,8 +1,8 @@
 import tasks.*;
 
 public interface TestObjects {
-    EpicTask epicTask1 = new EpicTask("EpicTask 1", "Description epic task");
-    EpicTask epicTask2 = new EpicTask("EpicTask 2", "Description epic task");
+    Epic epicTask1 = new Epic("Epic 1", "Description epic task");
+    Epic epicTask2 = new Epic("Epic 2", "Description epic task");
     Subtask subtask1ForEpicTask1 = new Subtask("sub1 for ep1", "Description sub1 for ep 1");
     Subtask subtask2ForEpicTask1 = new Subtask("sub2 for ep1", "Description sub2 for ep 1");
     Subtask subtask1ForEpicTask2 = new Subtask("sub1 for ep2", "Description sub1 for ep 2");
