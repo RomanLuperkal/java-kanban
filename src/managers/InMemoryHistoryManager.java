1515c360d45d5360287771a440a39ef77aca3dff
Roman <madara-09@bk.ru>
2022-07-06 23:57:33 +0300

    feat: реализовано сохранение истории

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -1,35 +1,84 @@
 package managers;
 
+
 import tasks.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> history;
+    private Node head;
+    private Node tail;
+    final private Map<Integer, Node> map;
 
     public InMemoryHistoryManager() {
-        history = new LinkedList<>();
+        map = new HashMap<>();
+    }
+
+    public void linkList(Task element) {
+        final Node oldTail = tail;
+        final Node newNode = new Node(oldTail, element, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.setNext(newNode);
+        }
+        map.put(element.getId(), newNode);
+    }
+
+    public List<Task> getTasks() throws IllegalStateException {
+        List<Task> tasksHistory = new ArrayList<>();
+        Node node = head;
+        if (node.getData() == null) {
+            throw new IllegalStateException("История просмотров пуста");
+        }
+        while (true) {
+            if (node == null) {
+                break;
+            }
+            tasksHistory.add(node.getData());
+            node = node.getNext();
+        }
+        return tasksHistory;
     }
 
     public List<Task> getHistory() throws IllegalStateException {
-        if (history.isEmpty()) {
-            throw new IllegalStateException("История просмотров пуста");
-        } else {
-            return history;
-        }
+        return getTasks();
     }
 
     public void add(Task task) {
-        addToHistory(task);
-    }
-
-    private void addToHistory(Task task) {
-        if (history.size() < 10) {
-            history.add(task);
+        if (map.containsKey(task.getId())) {
+            remove(task.getId());
+            linkList(task);
         } else {
-            ((LinkedList<Task>) history).removeFirst();
-            history.add(task);
+            linkList(task);
         }
     }
+
+    public void remove(int id) {
+        removeNode(map.get(id));
+    }
+
+    public void removeNode(Node node) {
+        Node prev = node.getPrev();
+        Node next = node.getNext();
+        if (prev == null) {
+            if (next != null) {
+                head = next;
+                head.setPrev(null);
+                return;
+            }
+            tail = null;
+            return;
+        }
+        if (next == null) {
+            prev.setNext(null);
+            tail = prev;
+            return;
+        }
+        prev.setNext(next);
+        next.setPrev(prev);
+    }
+
+
 }
