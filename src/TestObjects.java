1dc0475810e5d32b7eb39d66a17e3f6d88a35dbf
Roman <madara-09@bk.ru>
2022-07-07 00:01:06 +0300

    fix: обновленны тестовые объекты

--- src/TestObjects.java
+++ src/TestObjects.java
@@ -5,7 +5,5 @@ public interface TestObjects {
     Epic epicTask2 = new Epic("Epic 2", "Description epic task");
     Subtask subtask1ForEpicTask1 = new Subtask("sub1 for ep1", "Description sub1 for ep 1");
     Subtask subtask2ForEpicTask1 = new Subtask("sub2 for ep1", "Description sub2 for ep 1");
-    Subtask subtask1ForEpicTask2 = new Subtask("sub1 for ep2", "Description sub1 for ep 2");
-    Task task = new Task("Task", "Description Task");
-
+    Subtask subtask3ForEpicTask1 = new Subtask("sub3 for ep2", "Description sub3 for ep 2");
 }
