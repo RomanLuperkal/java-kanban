e08e2574acab26123e843630e0808552be01dcb8
Roman <madara-09@bk.ru>
2022-07-20 23:06:45 +0300

    refactor: добавлен тестовый объект

--- src/TestObjects.java
+++ src/TestObjects.java
@@ -1,6 +1,7 @@
 import tasks.*;
 
 public interface TestObjects {
+    Task task = new Task("Task", "Description task");
     Epic epicTask1 = new Epic("Epic 1", "Description epic task");
     Epic epicTask2 = new Epic("Epic 2", "Description epic task");
     Subtask subtask1ForEpicTask1 = new Subtask("sub1 for ep1", "Description sub1 for ep 1");
