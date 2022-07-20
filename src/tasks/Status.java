91bbac094f02d6ba65b5ea0970ffd498e11d303b
Roman <madara-09@bk.ru>
2022-07-20 23:03:39 +0300

    feat: реализован метод parseStatus()

--- src/tasks/Status.java
+++ src/tasks/Status.java
@@ -2,5 +2,17 @@ package tasks;
 
 public enum Status {
 
-    NEW, IN_PROGRESS, DONE
+    NEW, IN_PROGRESS, DONE;
+
+    public static Status parseStatus(String str) {
+        switch (str) {
+            case "IN_PROGRESS":
+                return Status.IN_PROGRESS;
+            case "DONE":
+                return Status.DONE;
+            case "NEW":
+                return Status.NEW;
+        }
+        return null;
+    }
 }
