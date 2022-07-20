639c23283d12ab60152367934c67f8b07e41ae5b
Roman <madara-09@bk.ru>
2022-07-20 22:59:01 +0300

    feat: добавлен новый коструктор fix: переписан toString()

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,17 +1,24 @@
 package tasks;
 
 public class Subtask extends Task {
-    public Subtask(String name, String description) {
+    private Integer epicID;
 
+    public Subtask(String name, String description) {
         super(name, description);
+    }
+
+    public Subtask(Integer id, String name, Status status, String description, Integer epicID) {
+        super(id, name, status, description);
+        this.epicID = epicID;
+    }
+
+    public void setEpicID(int epicID) {
+        this.epicID = epicID;
     }
 
     @Override
     public String toString() {
-        return "Subtask{" +
-                "name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", status=" + status +
-                '}';
+        return getId() + "," + Tasks.SUBTASK + "," + this.name + "," + this.status + "," + this.description + ","
+                + epicID;
     }
 }
