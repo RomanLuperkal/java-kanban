96ec9d08b50f00816f28445290259678a4a7ff01
Roman <madara-09@bk.ru>
2022-08-08 12:23:14 +0300

    fix: теперь класс наследуется от абстрактного класса Task

---
+++ src/tasks/SimpleTask.java
@@ -0,0 +1,58 @@
+package tasks;
+
+import exceptions.TaskDateDurationException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public class SimpleTask extends Task {
+
+
+    public SimpleTask(String name, String description) {
+        super(name, description);
+    }
+
+    public SimpleTask(String name, Status status, String description) {
+        super(name, status, description);
+    }
+
+    public SimpleTask(String name, Status status, String description, String DataTime, Integer durationMin) {
+        super(name, status, description);
+        this.startTime = LocalDateTime.parse(DataTime, formatter);
+        this.duration = Duration.ofMinutes(durationMin);
+    }
+
+    public SimpleTask(String name, String description, String DataTime, Integer durationMin) {
+        super(name, description);
+        LocalDateTime timeNow = LocalDateTime.now();
+        LocalDateTime DateTask = LocalDateTime.parse(DataTime, formatter);
+        if (timeNow.isAfter(DateTask)) {
+            throw new TaskDateDurationException("Время начала не может быть раньше нынешнего момента времени");
+        }
+        if (durationMin <= 0) {
+            throw new TaskDateDurationException("Продолжительность не может быть отрицательной либо равна 0");
+        }
+        this.startTime = DateTask;
+        this.duration = Duration.ofMinutes(durationMin);
+        this.status = Status.NEW;
+    }
+
+    public LocalDateTime getEndTime() {
+        if (startTime != null) {
+            return startTime.plus(duration);
+        }
+        return null;
+    }
+
+
+    @Override
+    public String toString() {
+        String startDateToString = Optional.ofNullable(startTime)
+                .map(localDateTime -> localDateTime.format(formatter)).orElse("null");
+        String durationToString = Optional.ofNullable(duration)
+                .map(duration -> Long.toString(duration.toMinutes())).orElse("null");
+        return getId() + "," + TaskType.TASK + "," + this.name + "," + this.status + "," + this.description + ","
+                + startDateToString + "," + durationToString;
+    }
+}
