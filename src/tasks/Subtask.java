807745083fe7c0e285c2fcce0b989fb291235ed0
Roman <madara-09@bk.ru>
2022-08-08 12:23:55 +0300

    feat: реализована продожлительность, время старт, время окончания

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,5 +1,12 @@
 package tasks;
 
+import exceptions.TaskDateDurationException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
 public class Subtask extends Task {
     private Integer epicID;
 
@@ -12,6 +19,36 @@ public class Subtask extends Task {
         this.epicID = epicID;
     }
 
+    public Subtask(String name, Status status, String description, String DataTime, Integer durationMin, Integer epicID) {
+        super(name, status, description);
+        this.startTime = LocalDateTime.parse(DataTime, formatter);
+        this.duration = Duration.ofMinutes(durationMin);
+        this.epicID = epicID;
+    }
+
+    public Subtask(String name, String description, String DataTime, Integer durationMin) {
+        super(name, description);
+        LocalDateTime timeNow = LocalDateTime.now();
+        LocalDateTime DateTask = LocalDateTime.parse(DataTime, formatter);
+        if (timeNow.isAfter(DateTask)) {
+            throw new TaskDateDurationException("Время начала не может быть раньше нынешнего момента времени");
+        }
+        if (durationMin <= 0) {
+            throw new TaskDateDurationException("Продолжительность не может быть отрицательной либо равна 0");
+        }
+        this.startTime = DateTask;
+        this.duration = Duration.ofMinutes(durationMin);
+        this.status = Status.NEW;
+    }
+
+    public LocalDateTime getEndTime() {
+        if (this.startTime != null) {
+            return startTime.plus(duration);
+        }
+        return null;
+    }
+
+
     public void setEpicID(int epicID) {
         this.epicID = epicID;
     }
@@ -21,8 +58,26 @@ public class Subtask extends Task {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Subtask subtask = (Subtask) o;
+        return epicID.equals(subtask.epicID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epicID);
+    }
+
+    @Override
     public String toString() {
+        String startDateToString = Optional.ofNullable(startTime)
+                .map(localDateTime -> localDateTime.format(formatter)).orElse("null");
+        String durationToString = Optional.ofNullable(duration)
+                .map(duration -> Long.toString(duration.toMinutes())).orElse("null");
         return getId() + "," + TaskType.SUBTASK + "," + this.name + "," + this.status + "," + this.description + ","
-                + epicID;
+                + this.epicID + "," + startDateToString + "," + durationToString;
     }
 }
