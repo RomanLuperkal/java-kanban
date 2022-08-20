f6d28d3c9e6c8df1ac6c5caddf899816f6397664
Roman <madara-09@bk.ru>
2022-08-21 00:48:51 +0300

    refactor: реализована проверка HTTPTaskManager

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,53 @@
+import com.google.gson.*;
+import managers.HTTPTaskManager;
 import managers.Managers;
 import managers.TaskManager;
 import managers.TestObjects;
-import tasks.Task;
+import servers.KVServer;
+import servers.adapters.*;
+import servers.HttpTaskServer;
+import tasks.*;
 
-import java.lang.IllegalStateException;
-import java.util.Random;
+import java.io.IOException;
 
 public class Main implements TestObjects {
 
     public static void main(String[] args) {
-        TaskManager inMemoryTaskManager = Managers.getDefault();
-        Random random = new Random();
         try {
-            inMemoryTaskManager.createEpicTask(epicTask1);
-            inMemoryTaskManager.createSubtask(epicTask1.getId(), subtask1ForEpicTask1);
-            inMemoryTaskManager.createSubtask(epicTask1.getId(), subtask2ForEpicTask1);
-            inMemoryTaskManager.createSubtask(epicTask1.getId(), subtask3ForEpicTask1);
-            inMemoryTaskManager.createEpicTask(epicTask2);
-            int choice;
-            for (int x = 0; x < 10000; x++) {
-                choice = random.nextInt(5) + 1;
-                callTask(choice, inMemoryTaskManager);
-            }
-            int i = 0;
-            for (Task task : inMemoryTaskManager.getHistory()) {
-                System.out.println(i + ": " + task);
-                i++;
-            }
-            System.out.println(inMemoryTaskManager.getHistory());
-            inMemoryTaskManager.deleteSubtask(subtask3ForEpicTask1.getId());
-            i = 0;
-            System.out.println("Удаляем subtask3ForEpicTask1");
-            for (Task task : inMemoryTaskManager.getHistory()) {
-                System.out.println(i + ": " + task);
-                i++;
-            }
-            inMemoryTaskManager.deleteEpicTask(epicTask1.getId());
-            System.out.println("Удаляем epicTask1");
-            i = 0;
-            for (Task task : inMemoryTaskManager.getHistory()) {
-                System.out.println(i + ": " + task);
-                i++;
-            }
-        } catch (IllegalStateException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+            KVServer kvServer = new KVServer();
+            kvServer.start();
+            HttpTaskServer server = new HttpTaskServer(Managers.getDefault("http://localhost:8078"));
+            server.start();
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            TaskAdapter deserializerTasks = new TaskAdapter("type");
+            deserializerTasks.registerBarnType(TaskType.TASK.toString(), SimpleTask.class);
+            deserializerTasks.registerBarnType(TaskType.EPIC.toString(), Epic.class);
+            deserializerTasks.registerBarnType(TaskType.SUBTASK.toString(), Subtask.class);
 
-    private static void callTask(int choice, TaskManager inMemoryTaskManager) {
-        switch (choice) {
-            case 1:
-                System.out.println(inMemoryTaskManager.getEpicTask(epicTask1.getId()));
-                break;
-            case 2:
-                System.out.println(inMemoryTaskManager.getEpicTask(epicTask2.getId()));
-                break;
-            case 3:
-                System.out.println(inMemoryTaskManager.getSubtask(subtask1ForEpicTask1.getId()));
-                break;
-            case 4:
-                System.out.println(inMemoryTaskManager.getSubtask(subtask2ForEpicTask1.getId()));
-                break;
-            case 5:
-                System.out.println(inMemoryTaskManager.getSubtask(subtask3ForEpicTask1.getId()));
-                break;
-        }
+            TaskManager manager = Managers.getDefault("http://localhost:8078");
+            manager.createSimpleTask(TestObjects.task);
+            manager.createEpicTask(TestObjects.epicTask1);
+            manager.createSubtask(TestObjects.epicTask1.getId(), TestObjects.subtask1ForEpicTask1);
+            manager.createEpicTask(TestObjects.epicTask2);
+            manager.createSubtask(TestObjects.epicTask2.getId(), TestObjects.subtask3ForEpicTask1);
+            manager.createSubtask(TestObjects.epicTask1.getId(), TestObjects.subtask2ForEpicTask1);
+            subtask1ForEpicTask1.changeStatus(Status.IN_PROGRESS);
+            manager.updateSubtask(subtask1ForEpicTask1);
+            manager.getSimpleTask(TestObjects.task.getId());
+            manager.getEpicTask(TestObjects.epicTask1.getId());
+            manager.getSubtask(TestObjects.subtask3ForEpicTask1.getId());
+            manager.getSubtask(TestObjects.subtask2ForEpicTask1.getId());
 
+            TaskManager manager2 = HTTPTaskManager.loadManager("http://localhost:8078");
+            System.out.println(manager.getHistory());
+            System.out.println(manager2.getHistory());
+            System.out.println(manager.getPrioritizedTasks());
+            System.out.println(manager2.getPrioritizedTasks());
+            System.out.println();
+            kvServer.stop();
+            server.stop();
+        } catch (InterruptedException | IOException e) {
+            System.out.println("Ошибка подключения к серверу");
+        }
     }
 }
